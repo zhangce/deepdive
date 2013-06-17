65ff06eb982256818427d31c98742cdb8cc05ab1
Ce Zhang <czhang@cs.wisc.edu>
2013-06-17 02:56:07 -0500

    test

--- elementary/test/fg/test_modelaccessor.h
+++ elementary/test/fg/test_modelaccessor.h
@@ -7,8 +7,8 @@ TEST (MODELACCESSOR_TEST, LOAD){
   ModelAccessor ma;
   ma.load("/tmp/deepdive/models.tsv");
 
-  EXPECT_EQ(ModelAccessor::model[0], 0.00001);
-  EXPECT_EQ(ModelAccessor::model[1], 0.00002);
-  EXPECT_EQ(ModelAccessor::model[2], 0.00003);
+  EXPECT_EQ(ModelAccessor::model[1], 1);
+  EXPECT_EQ(ModelAccessor::model[2], 0.001);
+  EXPECT_EQ(ModelAccessor::model[3], 0.001);
   
 }
